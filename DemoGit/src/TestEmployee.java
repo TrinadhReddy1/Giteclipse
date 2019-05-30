bb82235f8af7da89d6b236df81dc1edbf570993e
TrinadhReddy1 <tnr4242@gmail.com>
2019-05-30 16:52:30 +0530

    second

---
+++ DemoGit/src/TestEmployee.java
@@ -0,0 +1,12 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Employee e1=new Employee();
+       e1.setEmpId(111);
+       e1.setEmpName("Anil");
+       e1.setEmpsal(50000);
+	}
+
+}
